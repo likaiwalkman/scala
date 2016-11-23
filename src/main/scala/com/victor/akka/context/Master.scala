d0db44a80e372253d3f9e3ca4a556d22b04301bd
likai <likai@hys-inc.cn>
2016-11-24 01:35:46 +0800

    uncomment http module

--- src/main/scala/com/victor/akka/context/Master.scala
+++ src/main/scala/com/victor/akka/context/Master.scala
@@ -2,10 +2,7 @@
 package com.victor.akka.context
 
 import akka.actor.{Actor, Props}
-import akka.io.IO
 import com.victor.akka.actors.{Commander, Communicator, Operator}
-import com.victor.http.Server
-import spray.can.Http
 
 /**
   * 'context' actor where other children actors dwell in
@@ -15,12 +12,6 @@ class Master extends Actor {
   val commander = context.actorOf(Props(new Commander()), "commander")
   val messenger = context.actorOf(Props(new Communicator()), "messenger")
   val worker = context.actorOf(Props(new Operator()), "worker")
-
-  val httpServerHandler = context.actorOf(Props(new Server), "server")
-
-  implicit val system = context.system
-  IO(Http) ! Http.Bind(httpServerHandler, "0.0.0.0", port = 8080)
-  println("http bind over")
 
   override def receive: Receive = {
     case _ => {
--- src/main/scala/com/victor/http/Bootstrap.scala
+++ src/main/scala/com/victor/http/Bootstrap.scala
@@ -2,11 +2,12 @@ package com.victor.http
 
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
-import com.victor.akka.context.Master
 import spray.can.Http
+
 
 object Bootstrap extends App{
   val sys = ActorSystem.create("webapp")
-  val server = sys.actorOf(Props(new Master))
-
+  val server = sys.actorOf(Props(new Server))
+  implicit val system = sys
+  IO(Http) ! Http.Bind(server, "0.0.0.0", port = 8080)
 }
